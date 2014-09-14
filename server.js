b474471e015361493c56378824a2c7e701ac7137
Dan Gillespie <danielg@umd.edu>
2014-09-13 22:51:11 -0400

    woah

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ var static = require('node-static'),
   mongo = require('mongodb'),
   util = require('util'),
   http = require('http')
-  formidable = require('formidable');
+  formidable = require('formidable'),
+  ObjectID = require('mongodb').ObjectID;
 
 var fileServer = new static.Server('./public');
 var port = Number(process.env.PORT || 5000);
-var mongoUri = process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL ||
-      'mongodb://localhost/mydb';
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 
 require('http').createServer(function (req, resp) {
   if (req.url == '/save' && req.method.toLowerCase() == 'post') {
@@ -17,15 +16,37 @@ require('http').createServer(function (req, resp) {
     form.parse(req, function(err, fields, files) {
       mongo.Db.connect(mongoUri, function (err, db) {
         db.collection('svg', function(er, collection) {
-          collection.insert({'fields': fields, 'file':files[0]}, {safe:true}, function (er, rs) {});
+            collection.insert({'fields': fields}, {safe:true}, function (er, rs) {
+              if (!er) {
+                resp.write('http://' + req.headers.host + '/load/' + rs[0]['_id']);
+                resp.end();
+              } else {
+                resp.end(500);
+              }
+            });
         });
       });
     });
-      resp.write("This is where JSON GOES");
-      resp.end();
+      return;
   } else if (req.url.indexOf("/load") == 0 && req.method.toLowerCase() == 'get') {
-    resp.write("load");
-    resp.end();
+    var id = req.url.split('/');
+    var data;
+
+    mongo.Db.connect(mongoUri, function (err, db) {
+        db.collection('svg', function(er, collection) {
+              collection.findOne({_id: new ObjectID(id[2])},  function(er,rs) {
+                if (!er) {
+                  resp.write(rs.fields.data);
+                  resp.end();
+                } else {
+                  console.log(err + er);
+                  resp.end(500);
+                }
+              });
+
+        });
+    });
+    return;
   } else {
     req.addListener('end', function () {
       fileServer.serve(req, resp);
